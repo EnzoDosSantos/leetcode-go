cb3a55d17a8b457c05f49257eaaa98edfd97dbb6
EnzoDosSantos <enzodossantos364@gmail.com>
2024-09-17 21:51:06 -0300

    add same tree solution

---
+++ solutions/same-tree/solution.go
@@ -0,0 +1,30 @@
+package solutions
+
+// PROBLEM URL: https://leetcode.com/problems/same-tree/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func IsSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p == nil || q == nil {
+		return false
+	}
+
+	return p.Val == q.Val && IsSameTree(p.Left, q.Left) && IsSameTree(p.Right, q.Right)
+}
